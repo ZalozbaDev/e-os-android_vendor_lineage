5c09d632ec27be088e9c0f4813a92520bf962ef6
Romain Hunault <romain.hunault@e.email>
2020-05-27 15:25:12 +0000

    Add ecloud SMS support

--- config/common.mk
+++ config/common.mk
@@ -122,30 +122,31 @@ PRODUCT_PACKAGES += \
 # Required /e/ packages
 PRODUCT_PACKAGES += \
     GmsCore \
-		GsfProxy \
-		FakeStore \
-		com.google.android.maps.jar \
-		Mail \
-		BlissLauncher \
-		BlissIconPack \
-		MozillaNlpBackend \
-		OpenWeatherMapWeatherProvider \
-		AccountManager \
-		MagicEarth \
-		PdfViewer \
-		Camera \
-		eDrive \
-		Weather \
-		Notes \
-		Tasks \
-		NominatimNlpBackend \
-		DroidGuard \
-		LibreOfficeViewer \
-		OpenKeychain \
-		Message \
-		Browser \
-		BrowserWebView \
-		Apps
+    GsfProxy \
+    FakeStore \
+    com.google.android.maps.jar \
+    Mail \
+    BlissLauncher \
+    BlissIconPack \
+    MozillaNlpBackend \
+    OpenWeatherMapWeatherProvider \
+    AccountManager \
+    MagicEarth \
+    PdfViewer \
+    Camera \
+    eDrive \
+    Weather \
+    Notes \
+    Tasks \
+    NominatimNlpBackend \
+    DroidGuard \
+    LibreOfficeViewer \
+    OpenKeychain \
+    Message \
+    Browser \
+    BrowserWebView \
+    Apps \
+    ESmsSync
 
 # Required Lineage packages
 PRODUCT_PACKAGES += \
