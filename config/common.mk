2bfbc303e205295f33d64f09e453faa432bc4136
Amit Kumar <mr.doc10jl96@gmail.com>
2020-11-04 06:46:24 +0000

    Add default permissions for some apps on Q
    Change-Id: I93ad0fecaf2f4487de35ebd620e3c855cb862250
    
    
    (cherry picked from commit cbad97dacb6b5cd20f500d5b41db777596319a09)

--- config/common.mk
+++ config/common.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-sysconfig.xml
 
+# Pre-granted eos-permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/eos-permissions.xml:system/etc/default-permissions/eos-permissions.xml
+
 # Copy all Lineage-specific init rc files
 $(foreach f,$(wildcard vendor/lineage/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
