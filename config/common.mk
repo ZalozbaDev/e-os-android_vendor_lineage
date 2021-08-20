f75a80946ee288b45bc3c5e7ebd9fa1e69da73ef
Aayush Gupta <theimpulson@e.email>
2021-08-20 10:27:44 +0000

    vendor: config: Include configuration for partners
    (cherry picked from commit d448334378ea48bdd3dd3b69025856812714dc6e)

--- config/common.mk
+++ config/common.mk
@@ -214,3 +214,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Include configuration for partners
+$(call inherit-product-if-exists, vendor/partner/$(BUILD_PARTNER)/config/$(BUILD_PARTNER).mk)
