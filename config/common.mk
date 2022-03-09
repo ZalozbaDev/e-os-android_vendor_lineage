7dd5023e280f8eb76e6fc76632ca1a6ff7303f1b
merothh <git@merothh.com>
2022-03-09 11:04:30 +0530

    vendor/lineage: Drop partner gms makefiles
    * We dont intend to ship gms inline anyways

--- config/common.mk
+++ config/common.mk
@@ -215,4 +215,3 @@ include vendor/lineage/config/version.mk
 -include vendor/lineage-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
--include vendor/lineage/config/partner_gms.mk
